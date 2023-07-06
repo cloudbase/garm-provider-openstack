6d3fa21e1745c6dbfdb0bd3791de71ffe729e265
Michael Kuhnt <michael.kuhnt@mercedes-benz.com>
2023-07-06 22:04:01 +0200

    add config / extra spec toggle to enable debug mode on boot

--- vendor/github.com/cloudbase/garm-provider-common/util/util.go
+++ vendor/github.com/cloudbase/garm-provider-common/util/util.go
@@ -238,6 +238,10 @@ func GetCloudConfig(bootstrapParams params.BootstrapInstance, tools github.Runne
 		tempToken = *tools.TempDownloadToken
 	}
 
+	type ExtraSpecs struct {
+		EnableBootDebug bool `json:"enable_boot_debug"`
+	}
+
 	installRunnerParams := cloudconfig.InstallRunnerParams{
 		FileName:          *tools.Filename,
 		DownloadURL:       *tools.DownloadURL,
@@ -251,6 +255,7 @@ func GetCloudConfig(bootstrapParams params.BootstrapInstance, tools github.Runne
 		CallbackURL:       bootstrapParams.CallbackURL,
 		CallbackToken:     bootstrapParams.InstanceToken,
 		GitHubRunnerGroup: bootstrapParams.GitHubRunnerGroup,
+		EnableBootDebug:   bootstrapParams.UserDataOptions.EnableBootDebug,
 	}
 	if bootstrapParams.CACertBundle != nil && len(bootstrapParams.CACertBundle) > 0 {
 		installRunnerParams.CABundle = string(bootstrapParams.CACertBundle)
